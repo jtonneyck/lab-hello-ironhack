8961c8a081e8eb3fdecbae714f44cfc1082a9ec2
almightyblob <dominik.antunovic@gmail.com>
2020-01-13 15:57:01 +0100

    now with loops!

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Copytop from "./components/Copytop.js";
 import Button from "./components/Button.js";
 import Card from "./components/Card";
+import cardinfo from "./components/cardinfo.js";
 
 function App() {
   return (
@@ -15,26 +16,9 @@ function App() {
         <Button />
       </div>
       <div className="bottom">
-        <Card
-          image="/images/icon1.png"
-          headline="Declarative"
-          copy="React makes it painless to create interactive UIs."
-        />
-        <Card
-          image="/images/icon2.png"
-          headline="Components"
-          copy="Build encapsulated components that manage their state."
-        />
-        <Card
-          image="/images/icon3.png"
-          headline="Single-Way"
-          copy="A set of immutable values are passed to the components"
-        />
-        <Card
-          image="/images/icon4.png"
-          headline="JSX"
-          copy="Statically-typed, designed to run on modern browsers"
-        />
+        {cardinfo.map(x => (
+          <Card image={x.image} headline={x.headline} copy={x.copy} />
+        ))}
       </div>
     </div>
   );
---
+++ starter-code/src/components/cardinfo.js
@@ -0,0 +1,24 @@
+const cardinfo = [
+  {
+    image: "/images/icon1.png",
+    headline: "Declarative",
+    copy: "React makes it painless to create interactive UIs."
+  },
+  {
+    image: "/images/icon2.png",
+    headline: "Components",
+    copy: "Build encapsulated components that manage their state."
+  },
+  {
+    image: "/images/icon3.png",
+    headline: "Single-Way",
+    copy: "A set of immutable values are passed to the components."
+  },
+  {
+    image: "/images/icon4.png",
+    headline: "JSX",
+    copy: "Statically-typed, designed to run on modern browsers."
+  }
+];
+
+export default cardinfo;
