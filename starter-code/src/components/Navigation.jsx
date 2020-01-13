baab6fa428b3cb7916d0952e30b47e98e81ebda3
Monika <mswidzinska87@gmail.com>
2020-01-13 14:52:10 +0100

    navigation

---
+++ starter-code/src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Navigation.css"
+
+const Navigation = (props) => {
+    return (
+        <div className = "navigation">
+
+        <div>
+            <img src="./ironhack-logo.svg" alt=""/>
+        </div>
+        <div>
+            <img src="./menu-top.svg" alt=""/>
+        </div>
+
+        </div>
+    );
+}
+
+export default Navigation;
