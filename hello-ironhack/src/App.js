f7097320dc6627e99f85112620d5a238a1c7fcb6
Francis Bukenya <frankie.bukenya@gmail.com>
2020-03-30 16:39:26 +0200

    Add header

--- hello-ironhack/src/App.js
+++ hello-ironhack/src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Heading from './main';
+import './Main.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <nav className="navbar-nav">
+          <a href="/" className="nav-item">
+            <img src="./ironhack-logo.svg" alt="" className="navbar-brand" />
+          </a>
+        </nav>
+        <Heading />
+        <a className="main-link" href="/">
+          Awesome!
         </a>
       </header>
     </div>
