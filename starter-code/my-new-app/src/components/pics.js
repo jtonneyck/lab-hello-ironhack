0aaa92406637a2e947d91f373de7419ab7fbce06
unknown <damianosmakris@hotmail.com>
2020-03-30 12:34:42 +0200

    done

---
+++ starter-code/my-new-app/src/components/pics.js
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+// const Pics = (props)=>{
+//     return(
+//         <span>
+//         {props.data}
+//         </span>
+//     )
+// }
+
+// export default Pics
---
+++ starter-code/my-new-app/src/components/smallHead.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SmallHead = (props)=>{
+    return(
+    <h4>{props.data}</h4>
+    )
+}
+
+export default SmallHead
