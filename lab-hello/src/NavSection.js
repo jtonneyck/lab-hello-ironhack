c4685f69dc025c18f8b871355087819e68ef018f
felixvanriessen <felixvanriessengurung@gmail.com>
2020-03-30 13:07:34 +0200

    done

---
+++ lab-hello/src/NavSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nav(){
+    return (
+        <div className='nav'>
+        <img id='ih-icon' src='/ironhack-logo.svg' alt='IronHack Logo'></img>
+        <img id='menu-icon' src='/menu-top.svg' alt='Menu Icon'></img>
+    </div>
+    )
+}
+
+export default Nav
