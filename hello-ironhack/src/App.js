64a535e8fea44aaefe8e7ba857aec2eb708dcc48
Francis Bukenya <frankie.bukenya@gmail.com>
2020-03-30 23:16:14 +0200

    Add complete iteraion II

--- hello-ironhack/src/App.js
+++ hello-ironhack/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Heading from './Main';
 import './Main.css';
 import Card from './Card';
+import './Card.css';
 
 // import icon1 from '/public/icon1.png';
 
@@ -16,39 +17,40 @@ function App() {
           </a>
         </nav>
         <img src="/react-logo.svg" alt="" className="main-img" />
+
         <Heading />
         <a className="main-link" href="/">
           Awesome!
         </a>
       </header>
 
-      <div className="card">
+      <div className="card-container">
         <div className="card">
-          <img src="/icon1.png" alt="" className="image-img" />
+          <img src="/icon1.png" alt="" className="card-img" />
           <Card
             title="Declarative"
             text="React makes it painless to create interactive UIs"
           />
         </div>
         <div className="card">
-          <img src="/icon2.png" alt="" className="image-img" />
+          <img src="/icon2.png" alt="" className="card-img" />
           <Card
             title="Components"
-            text="React makes it painless to create interactive UIs"
+            text="Build encapsulated components that manage their state"
           />
         </div>
         <div className="card">
-          <img src="/icon3.png" alt="" className="image-img" />
+          <img src="/icon3.png" alt="" className="card-img" />
           <Card
             title="Single-Way"
-            text="React makes it painless to create interactive UIs"
+            text="A set of immutable values are passed to the components"
           />
         </div>
         <div className="card">
-          <img src="/icon4.png" alt="" className="image-img" />
+          <img src="/icon4.png" alt="" className="card-img" />
           <Card
             title="JSX"
-            text="React makes it painless to create interactive UIs"
+            text="Statically typed, designed to run on modern browsers"
           />
         </div>
       </div>
--- hello-ironhack/src/Card.js
+++ hello-ironhack/src/Card.js
@@ -3,10 +3,9 @@ import React, { Component } from 'react';
 class Card extends Component {
   render() {
     return (
-      <div className="image-card">
-        <img src="" alt="" className="image-img" />
-        <h3 className="image-card-heading">{this.props.title}</h3>
-        <p className="image-card-text">{this.props.text}</p>
+      <div className="card-content">
+        <h3 className="card-heading">{this.props.title}</h3>
+        <p class="card-text">{this.props.text}</p>
       </div>
     );
   }
