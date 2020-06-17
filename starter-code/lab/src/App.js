4c503a0e42c56089b5693f47a6c19a50717cd58c
Melanie N <melanie.nachef@gmail.com>
2020-06-17 15:08:55 +0200

    some improvements

--- starter-code/lab/src/App.js
+++ starter-code/lab/src/App.js
@@ -3,16 +3,22 @@ import './App.css';
 import Asset from './components/Asset';
 import FlexBox from './components/FlexBox';
 import Hello from './components/Hello';
+import assets from './assets.json'
+
 
 function App() {
   return (
     <div className="App">
       <Hello />
       <FlexBox>
-        <Asset src = "../images/icon1.png" name = "Declarative" description = "React makes it painless to create interactive UIs." />
-        <Asset src = "../images/icon2.png" name = "Components" description = "Build encapsulated components that manage their state." />
-        <Asset src = "../images/icon3.png" name = "Single-Way" description = "A set of immutable values are passed to the components's." />
-        <Asset src = "../images/icon4.png" name = "JSX" description = "Statically-typed, designed to run on modern browsers." />
+        {
+          assets.map(asset=>
+              <Asset
+                src = {asset.src}
+                name = {asset.name}
+                description = {asset.description} />
+          )
+        }
       </FlexBox>
     </div>
   );
--- starter-code/lab/src/components/Hello.js
+++ starter-code/lab/src/components/Hello.js
@@ -5,6 +5,13 @@ import './Hello.css';
 function Hello(props){
     return(
         <div className = 'hello'>
+            <div className = 'logo'>
+                <img src='./images/ironhack-logo.svg' alt="ironhack logo" />
+                <img src='./images/menu-top.svg' alt="menu" />
+            </div>
+            <div className="watermark">
+                <img src='./images/react-logo.svg' />
+            </div>
             <h1>Say hello to ReactJS</h1>
             <p>You will learn a Frontend framework from scratch, to become a Ninka Developer.</p>
             <a href="/">Awesome!</a>
